a844b8af16d7fbabe532530b200571a6d06b5d90
larry Caichihua <lcaichihua@hotmail.com>
2020-02-05 17:04:27 -0500

    config add

--- src/main/java/com/cavetech/springboot/ApiGateway/app/SpringbootApiGatewayApplication.java
+++ src/main/java/com/cavetech/springboot/ApiGateway/app/SpringbootApiGatewayApplication.java
@@ -1,9 +1,13 @@
 package com.cavetech.springboot.ApiGateway.app;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
